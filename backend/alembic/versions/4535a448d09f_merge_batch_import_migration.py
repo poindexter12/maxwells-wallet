bf533eb6ed08b21ba2c1004e9bff0b5ddc3bb296
poindexter12 <poindexter12@gmail.com>
2025-11-29 16:41:40 -0800

    chore: merge alembic heads for batch_import

---
+++ backend/alembic/versions/4535a448d09f_merge_batch_import_migration.py
@@ -0,0 +1,24 @@
+"""merge batch_import migration
+
+Revision ID: 4535a448d09f
+Revises: 2b9c8a09f9d4, ea943ea0e6ab
+Create Date: 2025-11-29 16:41:18.460692
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4535a448d09f'
+down_revision = ('2b9c8a09f9d4', 'ea943ea0e6ab')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
