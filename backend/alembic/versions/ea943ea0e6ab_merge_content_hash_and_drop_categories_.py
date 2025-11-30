b7383cd3127ea5e7befbae4918526811d4a16835
poindexter12 <poindexter12@gmail.com>
2025-11-29 16:34:13 -0800

    chore: merge alembic heads for content_hash and drop_categories

---
+++ backend/alembic/versions/ea943ea0e6ab_merge_content_hash_and_drop_categories_.py
@@ -0,0 +1,24 @@
+"""merge content_hash and drop_categories migrations
+
+Revision ID: ea943ea0e6ab
+Revises: 2d2f32056af5, dbcc12063912
+Create Date: 2025-11-29 16:32:57.134243
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ea943ea0e6ab'
+down_revision = ('2d2f32056af5', 'dbcc12063912')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
